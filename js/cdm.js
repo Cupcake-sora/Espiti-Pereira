0f862ce0b3016aba94f74eebeb0cec3b654dc2b9
Ana <pereirarojasanab@gmail.com>
2022-07-28 20:38:28 -0300

    correccion de errores de ingreso

--- js/cdm.js
+++ js/cdm.js
@@ -1,50 +1,83 @@
 let usuarioRegistrado = 'cupcake';
 
-switch (opcion) {
-    case value:
-        
-        break;
+alert('Bienvenido a CDM, un juego de rol donde tu eliges el rumbo de la historia');
 
-    default:
-        break;
-}
+let salir = false
 
-function registro() {
-    let ingresar = false;
+//OPCION 1
+function ingresar() {
+    let ingresa = false;
 
     for (let i = 2; i >= 0; i--) {
         let ponerUsuario = prompt('Ingrese su usuario, tiene ' + (i + 1) + ' oportunidades.');
-        if (ponerUsuario === usuario) {
-            alert('Bienvenido/a ' + usuario + ' <3');
+        if (ponerUsuario === usuarioRegistrado) {
+            alert('Bienvenido/a ' + usuarioRegistrado + ' <3');
             ingresar = true;
+            salir = true;
             break;
         } else {
             alert('Mmmm, volve a intentarlo, te quedan ' + i + ' oportunidades.');
         }
     }
-
-    return ingresar;
 }
 
-function Ingreso() {
+//OPCION 2
+function registrarse() {
+    //ingresar el nombre
     let nombre = prompt('Ingresa tu nombre');
-    let cambiarNomUsuario = prompt('El nombre ingresado es ' + nombre + ' eso es correcto?  \n1- Si, es correcto,  \n2- No, quiero cambiarlo.');
-    switch (cambiarNomUsuario) {
+    let cambiar = false;
+    while (cambiar == false) {
+        let cambiarNombre = prompt('El nombre ingresado es " ' + nombre + ' " eso es correcto? \n1- Si, es correcto \n2- No, quiero cambiarlo.');
+
+        switch (cambiarNombre) {
+            case '1':
+                cambiar = true;
+                alert('Correcto tu nombre es ' + nombre);
+                break;
+            case '2':
+                nombre = prompt('Ingresa tu nombre nuevamente');
+                break;
+            default:
+                alert('Opción invalida.');
+        }
+    }
+
+    //ingresar un nombre de usuario
+    let nuevoUsuario = prompt('Ingrese un nombre de usuario');
+    let registroNuevoUsuario = true;
+    while (registroNuevoUsuario == true) {
+
+        if (nuevoUsuario != usuarioRegistrado) {
+            alert('Yei, ese nombre de usuario esta disponible');
+            alert('Bienvenido/a ' + nuevoUsuario + ' <3');
+            registroNuevoUsuario = false;
+        }
+        else {
+            alert('Mmmm ese usuario no esta disponible, volve a intentarlo');
+            nuevoUsuario = prompt('Ingresa otro nombre de usuario por favor');
+        }
+    }
+
+    alert(nombre + ' : \nTu nombre se usuario es  ' + nuevoUsuario + ('\nEsta va a ser tu clave para ingresar a CDM. \nPor favor no compartas esta informacion, ya que podrias ser victima de un robo de cuenta'));
+    salir = true;
+}
+
+
+
+//ESTO ES EL MENU PRINCIPAL
+while (salir == false) {
+    switch (opcion = prompt('Por favor ingrese una opción: \n1- Ingresar \n2- Registrarme \n3- Salir')) {
         case '1':
-            alert('Correcto' + nombre);
+            ingresar();
             break;
         case '2':
+            registrarse();
             break;
-
-        default:
+        case '3':
+            salir = true;
+            alert('Ha salido con exito');
+            break;
+        default: alert('Opcion invalida');
             break;
     }
-
-
-    let nuevoUsuario = prompt('Ingrese un nombre de usuario')
-    if (nuevoUsuario != usuarioRegistrado) {
-        alert('Yei, ese nombre de usuario esta disponible');
-        alert('Bienvenido/a ' + nuevoUsuario + ' <3');
-    }
-    alert('Bienvenido/a, ' + dato);
 }
